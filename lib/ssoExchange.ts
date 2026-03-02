afc128c7a9eb95d8eeb8473a42df84c7ff9f1290
Nizam Yuseri <nizamyuseri@MacBook-Air-Nizam.local>
2026-03-02 13:23:04 +0800

    add signup user if not exist

--- lib/ssoExchange.ts
+++ lib/ssoExchange.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { api } from './api'
 import { useNavigate } from 'react-router-dom';
+import { supabase } from './supabase';
 
 export function useSsoExchange() {
   const navigate = useNavigate();
@@ -8,9 +9,23 @@ export function useSsoExchange() {
     queryKey: ['sso-exchange'],
     queryFn: async () => {
       try {
-        const { data } = await api.get('/sso/exchange')
+        const { data: sessionData } = await supabase.auth.getSession();
+        const { data } = await api.get('/sso/exchange');
+        console.log('sessionData response:', sessionData);
         return data
       } catch (error) {
+      //   const fallback = {
+      //   user_id: supabaseUserId,
+      //   email: user?.email || '',
+      //   name: user?.name || '',
+      //   account_type: user?.accountType || 'individual',
+      //   phone: user?.phone || '',
+      //   position: user?.position || '',
+      //   company_name: user?.clinicName || null,
+      //   avatar_url: finalAvatar,
+      //   background_url: finalBackground
+      // };
+      // const { error: upsertError } = await supabase.from('profiles').upsert(fallback, { onConflict: 'user_id' });
         console.log('error during SSO exchange, likely no active session:', error);
         // navigate('/login', { replace: true });
         throw error
