4067d8460f7ba6a4022c841ba811f1d1ed88558e
Nizam Yuseri <nizamyuseri@MacBook-Air-Nizam.local>
2026-09-21 21:14:29 +0800

    revert back to sso

--- lib/loginOdoo.ts
+++ lib/loginOdoo.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://app.snabbb.com/api",
+  baseURL: "https://sso.snabbb.com/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
